2489421509d62cc2aa1e43688dbabd4f40af773e
maulik-kanzariya-prominentpixel <maulik.kanzariya@prominentpixel.com>
2025-12-31 18:17:29 +0530

    REMOVED : removed .env credentials

--- server/src/config/dynamodb.client.ts
+++ server/src/config/dynamodb.client.ts
@@ -1,21 +1,9 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-let client;
-
-if (process.env.NODE_ENV == "dev") {
-  console.log("{local creds}");
-  client = new DynamoDBClient({
-    region: "us-east-1",
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-      sessionToken: process.env.AWS_SESSION_TOKEN!,
-    },
-  });
-} else {
-  client = new DynamoDBClient();
-}
+let client = new DynamoDBClient({
+  region: "us-east-1",
+});
 
 const docClient = DynamoDBDocumentClient.from(client);
 
